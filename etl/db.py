5aae969bdc2f7b3257b892c7efd0e37d46e0f2cc
ThorfinnThor <pixsatmaps@gmail.com>
2025-09-10 15:27:27 +0200

    Update db.py

--- etl/db.py
+++ etl/db.py
@@ -1,9 +1,10 @@
 # etl/db.py
 import os
 import psycopg2
+from dotenv import load_dotenv
+
+load_dotenv()
 
 def get_db_connection():
-    database_url = os.environ.get("DATABASE_URL")
-    if not database_url:
-        raise RuntimeError("DATABASE_URL is not set")
-    return psycopg2.connect(database_url, sslmode="require")
+    return psycopg2.connect(os.getenv("DATABASE_URL"))
+
