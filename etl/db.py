0e6047975710812067b75c32292266972fa85dcd
ThorfinnThor <pixsatmaps@gmail.com>
2025-09-10 16:14:00 +0200

    Update db.py

--- etl/db.py
+++ etl/db.py
@@ -1,17 +1,7 @@
-# etl/db.py
-import os
-import psycopg2
+import os, psycopg2
 from dotenv import load_dotenv
 
 load_dotenv()
 
 def get_db_connection():
-    conn = psycopg2.connect(
-        host="db.wahqfdgybivndsplphro.supabase.co",  # force IPv4 resolution
-        port=5432,
-        dbname=os.getenv("DB_NAME", "postgres"),
-        user=os.getenv("DB_USER", "postgres"),
-        password=os.getenv("DB_PASSWORD"),
-        sslmode="require"  # required by Supabase
-    )
-    return conn
+    return psycopg2.connect(os.getenv("DATABASE_URL"))
